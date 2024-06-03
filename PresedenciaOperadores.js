5511b36a730967c921e8f20a6cf3db598d295b8d
ChrisferDev <ferchris82@hotmail.com>
2024-06-03 14:50:59 -0500

    Versión 1.0

---
+++ PresedenciaOperadores.js
@@ -0,0 +1,18 @@
+// Presedencia Operadores 
+
+// 1. Parentesis y Corchetes 
+// 2. Operadores unarios, como -, ++, --, ! 
+// 3. Aritmeticos *, / y % 
+// 4. Aritmeticos + y - 
+// 5. Relacionales <, <=, > y >= 
+// 6. Igualdad == y !=
+// 7. Logicos && y || 
+// 4. Asignacion =, =+, -=, *=, etc
+
+// Ej. Se revisa de izq a der
+let a = 12 / 3 + 2 * 3 - 1;
+// Paso 1. División 12 / 3 = 4
+// Paso 2. Multiplicación 2 * 3 = 6
+// Paso 3. Suma 4 + 6 = 10 
+// Paso 4. Resta 10 - 1 = 9
+console.log(a);
