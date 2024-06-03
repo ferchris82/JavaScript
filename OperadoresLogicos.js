0f22d53aab6e14b5f1c23c4ea23393a3ac93a53e
ChrisferDev <ferchris82@hotmail.com>
2024-06-03 14:32:52 -0500

    Versión 1.0

---
+++ OperadoresLogicos.js
@@ -0,0 +1,18 @@
+// Operadores Lógicos
+let a = true;
+let b = false;
+console.log(a);
+console.log(b);
+
+// Operador logico && (and o Y)
+// Regresa verdadero si ambos operandos
+// son verdaderos 
+console.log(`${a} && ${b} -> ${a && b}`);
+
+// Operador logico || (or o O)
+// Resumen verdadero si cualquiera de los valores es verdadero
+console.log(`${a} || ${b} -> ${a || b}`);
+
+// Operador ! (not o NO)
+// Invierte el valor original, true -> false, false -> true
+console.log(`${a} -> ${!a}`);
