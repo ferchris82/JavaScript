d96003d12a4f2deab15e59cc0a9a03b32f65a897
ChrisferDev <ferchris82@hotmail.com>
2024-06-03 13:46:11 -0500

    Versión 1.0

---
+++ OperadoresRelacionales.js
@@ -0,0 +1,16 @@
+// Operadores de Relacionales (comparación)
+let a = 5;
+let b = '5';
+console.log(a);
+console.log(b);
+
+// Operadores igualdad ==
+// (Solo compara valores, y hace una conversion si es necesario)
+console.log("a == b -> ", a == b);
+// String interpolation
+console.log(`${a} == ${b} -> ${a == b}`);
+// Operador igualdad estricta o exacto
+// Se compara el valor y el tipo de dato
+console.log(" a === b -> ", a === b);
+// String interpolation
+console.log(`${a} === ${b} -> ${a === b}`);
