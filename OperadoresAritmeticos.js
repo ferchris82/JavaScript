636229056cd0cb19f1c594b0ff5010c100af1d19
ChrisferDev <ferchris82@hotmail.com>
2024-06-03 13:10:43 -0500

    first commit

---
+++ OperadoresAritmeticos.js
@@ -0,0 +1,30 @@
+// Operadores Aritmeticos
+let a,b,c,d,e,f;
+
+// Suma 
+a = 3 + 4;
+console.log(a);
+
+// Resta
+b = 6 - 2;
+console.log(b);
+
+// Multiplicación
+c = a * 2;
+console.log(c);
+
+// División 
+d = b / 2.5;
+console.log(d);
+console.log(typeof d);
+
+// Modulo o residuo
+f = 9 % 2;
+console.log(f);
+
+//Potencia (ES2016 - ECMAScript 2016)
+e = 2 ** 3; // 2 * 2 * 2 = 8 
+console.log(e);
+
+e = Math.pow(2,3);
+console.log(e);
---
+++ variables.js
@@ -0,0 +1,18 @@
+//Reglas de nombres de variables
+
+//Letras, $, _, posteriormente se pueden usar numeros
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
+
+//JavaScript es sensible a may/min
+// console.log(MiNumero3);
+
+/*
+varias lineas 
+ */
