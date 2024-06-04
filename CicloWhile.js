3ffa2d8da4c860ca842637abb39e96d3d80b7811
ChrisferDev <ferchris82@hotmail.com>
2024-06-04 12:54:41 -0500

    Versión 1.0

---
+++ CicloWhile.js
@@ -0,0 +1,8 @@
+// Sentencia ciclo while
+// Imprimir los valores del 0 al 5
+let contador = 0, repeticiones = 5;
+
+while(contador <= repeticiones){
+    console.log(contador++);
+    //contador++;
+}
