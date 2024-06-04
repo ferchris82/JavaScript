b9fb550ef8f923726072036e8311917aef5ac66a
ChrisferDev <ferchris82@hotmail.com>
2024-06-04 11:58:20 -0500

    Versión 1.0

---
+++ SentenciaCondicionales.js
@@ -0,0 +1,14 @@
+// Sentencia if
+let miNumero = -10;
+
+// Revisar si el numero es positivo
+if(miNumero > 0){
+    console.log(`Valor positivo ${miNumero}`);
+}
+else if (miNumero < 0) {
+    console.log(`Valor negativo ${miNumero}`);
+}
+
+else{
+    console.log(`Valor cero o negativo ${miNumero}`);
+}
