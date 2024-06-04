82242c37c303e17e327c1f46467d17823f2e1e7d
ChrisferDev <ferchris82@hotmail.com>
2024-06-04 16:00:10 -0500

    Versión 1.0

---
+++ Funciones.js
@@ -0,0 +1,11 @@
+// Funciones en JS
+// 1. Declarar la función
+function sumar(a, b){
+    let resultadoSuma = a + b;
+    return resultadoSuma;
+}
+
+// 2. Llamar o invocar a la función 
+let argA = 5, argB = 7;
+let resultado = sumar(argA, argB);
+console.log(`Resultado suma: ${resultado}`);
