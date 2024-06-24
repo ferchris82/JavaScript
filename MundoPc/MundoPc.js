95524af4137fd66dee2331f3524eceb512109acf
ChrisferDev <ferchris82@hotmail.com>
2024-06-24 14:50:11 -0500

    Versión 1.0

--- MundoPc/MundoPc.js
+++ MundoPc/MundoPc.js
@@ -54,6 +54,39 @@ class Teclado extends DispositivoEntrada{
     }
 }
 
+class Monitor{
+    static contadorMonitores = 0;
+
+    constructor(marca, tamaño){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamaño = tamaño;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    toString(){
+        return `Monitor: [idMonitor: ${this._idMonitor}, marca: ${this._marca}, tamaño: ${this.tamaño}]`;
+    }
+}
+
+class Computadora{
+    static contadorComputadoras = 0;
+
+    constructor(nombre, monitor, raton, teclado){
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor;
+        this._raton = raton;
+        this._teclado = teclado;
+    }
+    toString(){
+        return `Computadora ${this._idComputadora}: ${this._nombre} \n ${this._monitor} \n ${this._raton} \n ${this._teclado}`;
+    }
+}
+
 let raton1 = new Raton('USB', 'HP');
 console.log(raton1.toString());
 let raton2 = new Raton('Bluetooth', 'Dell');
@@ -64,3 +97,13 @@ let teclado1 = new Teclado('Bluetooth', 'MSI');
 let teclado2 = new Teclado('USB', 'Acer');
 console.log(teclado1.toString());
 console.log(teclado2.toString()); 
+
+let monitor1 = new Monitor('HP', 15);
+let monitor2 = new Monitor('Dell', 27);
+console.log(monitor1.toString());
+console.log(monitor2.toString());
+
+let computadora1 = new Computadora('HP', monitor1, raton1, teclado1);
+console.log(`${computadora1}`);
+let computadora2 = new Computadora('Armada', monitor2, raton2, teclado2);
+console.log(`${computadora2}`)
