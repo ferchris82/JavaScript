54eb0e0a9ded630141ba68a6e0401586c3568f7e
ChrisferDev <ferchris82@hotmail.com>
2024-06-28 15:09:28 -0500

    Versión 1.0

---
+++ Aplicación de Presupuesto/js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Salario', 2100000.00),
+    new Ingreso('Venta de Software', 300.000)
+];
+
+const egresos = [
+    new Egreso('Administración', 100.000),
+    new Egreso('Netflix', 20.000)
+];
+
+let cargarApp = () =>{
+    cargarCabecero();
+}
+
+let cargarCabacero = () =>{
+    
+}
