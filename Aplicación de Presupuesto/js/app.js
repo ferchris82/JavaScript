7eda58df63dd652046ee3e40ef189a530dc4694e
ChrisferDev <ferchris82@hotmail.com>
2024-06-29 12:17:49 -0500

    Versión 1.0

--- Aplicación de Presupuesto/js/app.js
+++ Aplicación de Presupuesto/js/app.js
@@ -1,6 +1,7 @@
 const ingresos = [
-    new Ingreso('Salario', 2000000.00),
-    new Ingreso('Venta de Software', 300.000)
+    new Ingreso('Sueldo', 2000000),
+    new Ingreso('Venta de Software', 300000),
+    new Ingreso('Redes Sociales', 1500000)
 ];
 
 const egresos = [
@@ -8,29 +9,30 @@ const egresos = [
     new Egreso('Netflix', 20.000)
 ];
 
-let cargarApp = () =>{
+let cargarApp = () => {
     cargarCabecero();
+    cargarIngresos();
 }
 
-let totalIngresos = () =>{
+let totalIngresos = () => {
     let totalIngreso = 0;
-    for(let ingreso of ingresos){
+    for (let ingreso of ingresos) {
         totalIngreso += ingreso.valor;
     }
     return totalIngreso;
 }
 
-let totalEgresos = () =>{
+let totalEgresos = () => {
     let totalEgreso = 0;
-    for(let egreso of egresos){
+    for (let egreso of egresos) {
         totalEgreso += egreso.valor;
     }
     return totalEgreso;
 }
 
-let cargarCabecero = () =>{
+let cargarCabecero = () => {
     let presupuesto = totalIngresos() - totalEgresos();
-    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
     document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
     document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
     document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
@@ -41,6 +43,31 @@ const formatoMoneda = (valor) => {
     return valor.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 2 });
 }
 
-const formatoPorcentaje = (valor)=>{
-    return valor.toLocaleString('es-CO', {style: 'percent', minimumFractionDigits: 2});
+const formatoPorcentaje = (valor) => {
+    return valor.toLocaleString('es-CO', { style: 'percent', minimumFractionDigits: 2 });
+}
+
+const cargarIngresos = () => {
+    let ingresosHTML = '';
+    for (let ingreso of ingresos) {
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML = (ingreso) => {
+    let ingresoHTML = `
+    <div class="elemento limpiarEstilos">
+                    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+                    <div class="derecha limpiarEstilos">
+                        <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
+                        <div class="elemento_eliminar">
+                            <button class="elemento_eliminar--btn">
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+    `;
+    return ingresoHTML;
 }
